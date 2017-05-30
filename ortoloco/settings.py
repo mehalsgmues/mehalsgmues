1af06ed979497fbc9956930ef4ac493a91049a92
David Glenck <pascal.free@gmail.com>
2017-05-30 21:02:35 +0200

    second attempt

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -103,18 +103,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/staticadmin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+#"/static/",
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
